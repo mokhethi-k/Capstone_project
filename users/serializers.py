15cc3ef99f4f7ea2c7a23cbd421023b3f8024277
mokhethi-k <mokhethikabelo@gmail.com>
2025-08-26 20:44:18 +0200

    Implementing custom user data validation and creating profile viewset

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,32 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Profile
+
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['url', 'id', 'user', 'profile_picture']
 
 class UserSerializer(serializers.ModelSerializer):
     #customising user creation
     username = serializers.CharField(read_only=True)
     password = serializers.CharField(write_only=True, required=False)
+    old_password = serializers.CharField(write_only=True, required=False)
+    profile = ProfileSerializer(read_only=True)
+
+    def validate(self, data):
+        request_method = self.context['request'].method
+        password = data.get('password', None)
+        if request_method == 'POST':
+            if password == None:
+                raise serializers.ValidationError({'info':"Please provide a password!"},)
+        elif request_method == 'PUT' or request_method == 'PATCH':
+            old_password = data.get('old_password', None)
+            if old_password != None and old_password == None:
+                raise serializers.ValidationError({'info': 'Please provide the old password'},)
+        return data
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -14,7 +36,21 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
     
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            if 'password' in validated_data:
+                password = validated_data.pop('password')
+                old_password = validated_data.pop('old_password')
+                if user.check_password(old_password):
+                    user.set_password(password)
+                else:
+                    raise Exception("Old password is incorect!")
+                user.save()
+        except Exception as err:
+            raise serializers.ValidationError(err)
+        return super(UserSerializer, self).update(instance, validated_data)
 
     class Meta:
         model = User
-        fields = ['url','url', 'id', 'username', 'email', 'first_name', 'last_name', 'email', 'password']
+        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'password', 'old_password', 'profile']
--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ProfileSerializer
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .permissions import UserPermisions
+from .models import Profile
+
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [UserPermisions,]
     queryset = User.objects.all()
     serializer_class  = UserSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
