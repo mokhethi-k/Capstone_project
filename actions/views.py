37eb51fa1907431db5bec3f02a6cf9d34ffa8874
mokhethi-k <mokhethikabelo@gmail.com>
2025-08-27 21:20:22 +0200

    Implemetnting authentication to allow users to view tags when they are only logged -in

--- actions/views.py
+++ actions/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import RepairTagAction
 from .serializers import RepairTagActionSerializer
 
@@ -6,4 +6,4 @@ from .serializers import RepairTagActionSerializer
 class RepairTagActionViewSet(viewsets.ModelViewSet):
     queryset = RepairTagAction.objects.all().order_by('-created_at')
     serializer_class = RepairTagActionSerializer
-
+    permission_classes = [permissions.IsAuthenticated]
--- tags/views.py
+++ tags/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import RepairTag
 from .serializers import RepairTagSerializer
 
@@ -6,4 +6,10 @@ from .serializers import RepairTagSerializer
 class RepairTagViewSet(viewsets.ModelViewSet):
     queryset = RepairTag.objects.all().order_by('-created_at')
     serializer_class = RepairTagSerializer
+    permission_classes = [permissions.IsAuthenticated]
     
+    def perform_create(self, serializer):
+        """
+        Automatically set created_by to the current user
+        """
+        serializer.save(created_by=self.request.user)
