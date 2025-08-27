cec831c59c71cf4c65e66a59c22fe92f5c0032ad
mokhethi-k <mokhethikabelo@gmail.com>
2025-08-27 18:48:11 +0200

    create repair repair tag action model and customize the model view

---
+++ actions/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import RepairTagAction
+
+class RepairTagAdmin(admin.ModelAdmin):
+    list_display = ['tag', 'assigned_to', 'assigned_by',  'created_at', 'status', 'complete_at']
+    
+admin.site.register(RepairTagAction, RepairTagAdmin)
---
+++ actions/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tags.models import RepairTag  
+
+
+class RepairTagAction(models.Model):
+    STATUS_CHOICES = [
+        ('Not Started', 'Not Started'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    tag = models.ForeignKey(
+        RepairTag,
+        on_delete=models.CASCADE,
+        related_name="actions"
+    )
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="assigned_actions"
+    )
+    assigned_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="created_actions"
+    )
+    evidence = models.TextField(blank=True, null=True)  
+    evidence_file = models.FileField(upload_to="evidence/", blank=True, null=True)
+    evidence_image = models.ImageField(upload_to="evidence/images/", blank=True, null=True)
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Not Started")
+    created_at = models.DateTimeField(auto_now_add=True)
+    complete_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Action for Tag {self.tag.id} - {self.status}"
+
